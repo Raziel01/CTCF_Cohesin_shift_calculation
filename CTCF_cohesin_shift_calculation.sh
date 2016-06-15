bbc7058f8eb4b46f390e3a2b02523a3cd1073778
Raziel01 <czipa.erik@med.unideb.hu>
2016-06-15 14:34:50 +0200

    Update CTCF_cohesin_shift_calculation.sh

--- CTCF_cohesin_shift_calculation.sh
+++ CTCF_cohesin_shift_calculation.sh
@@ -8,38 +8,47 @@ read -p 'Cohesin 1. BEDGRAPH.GZ name: ' file_graph2 ;
 read -p 'Cohesin 2. BEDGRAPH.GZ name: ' file_graph3 ;
 read -p 'Motif file name: ' motif_file ;
 
-bed1_base=`basename $file1 .bed`;
-bed2_base=`basename $file2 .bed`;
-bed3_base=`basename $file3 .bed`;
-bedgraph1_base=`basename $file_graph1 .bedgraph.gz`;
-bedgraph2_base=`basename $file_graph2 .bedgraph.gz`;
-bedgraph3_base=`basename $file_graph3 .bedgraph.gz`;
+bed_base1=`basename $file1 .bed`;
+bed_base2=`basename $file2 .bed`;
+bed_base2=`basename $file3 .bed`;
+bedgraph_base1=`basename $file_graph1 .bedgraph.gz`;
+bedgraph_base2=`basename $file_graph2 .bedgraph.gz`;
+bedgraph_base2=`basename $file_graph3 .bedgraph.gz`;
 
-if [ ! -d tmp ] ; then
+if [ ! -d tmp0120231 ] ; then
         /bin/mkdir -p tmp0120231
 fi
 
 for ((i=1;i<=3;i+=1)); do
-        if [ ! -d bed${i}_base ] ; then
-                 /bin/mkdir -p bed${i}_base
+        if [ ! -d bed_base${i} ] ; then
+                 /bin/mkdir -p bed_base${i}
 fi
-        PeakSplitter -p file${i}  -w file_graph$i -f -o bed${i}_base;
-        awk '{OFS="\t"; if (NR>1) print $1,$5,$5+1,$1":"$5"-"$5+1,$5}' bed${i}_base/*.subpeaks.bed > bed${i}_base_summits.bed;
-        awk '{OFS="\t"; print $1,$2-50,$3+50,$4,$5}' bed${i}_base_summits.bed > tmp0120231/bed${i}_base_summits_motker.tmp
+        v=`echo 'file'$i`;
+        peak=${!v};
+        bname=`basename $peak .bed`
+        v2=`echo 'file_graph'$i`;
+        graph=${!v2};
+        echo  $graph;
+        cat ${peak} | sort -k1,1 -k2,2n  > tmp0120231/bed_base${i}_sorted.bed;
+        PeakSplitter -p tmp0120231/bed_base${i}_sorted.bed  -w $graph -f -o bed_base${i};
+        awk '{OFS="\t"; if (NR>1) print $1,$5,$5+1,$1":"$5"-"$5+1,$5}' bed_base${i}/*.subpeaks.bed > bed_base${i}_summits.bed;
+        awk '{OFS="\t"; if (NR>1) print $1,$5,$5+1,$1":"$5"-"$5+1,$5}' bed_base${i}/*.subpeaks.bed > ${bname}_summits.bed
+        awk '{OFS="\t"; print $1,$2-50,$3+50,$4,$5}' bed_base${i}_summits.bed > tmp0120231/bed_base${i}_summits_motker.tmp
+        rm -rf bed_base${i};
 done;
 
 
-intersectBed -a $motif_file -b  tmp/bed1_base_summits_motker.tmp -wa |  intersectBed -a stdin  -b  tmp/bed2_base_summits_motker.tmp -wa |  intersectBed -a stdin  -b  tmp/bed3_base_summits_motker.tmp -wa |   sort -k1,1 -k2,2n >   tmp0120231/CTCF_motif.bed;
+intersectBed -a $motif_file -b  tmp0120231/bed_base1_summits_motker.tmp -wa |  intersectBed -a stdin  -b  tmp0120231/bed_base2_summits_motker.tmp -wa |  intersectBed -a stdin  -b  tmp0120231/bed_base3_summits_motker.tmp -wa |   sort -k1,1 -k2,2n >   tmp0120231/CTCF_motif.bed;
 
 
 for ((i=1;i<=3;i+=1)); do
-        sort -k1,1 -k2,2n  bed${i}_base_summits.bed |  closestBed -a tmp/CTCF_motif.bed  -b stdin  > tmp0120231/bed${i}_base_closest.bed;
-        awk -F"\t" '{OFS="\t"; if ($6 == "+") print $4,$8-$2-8; else print $4,$2-$8+8}' tmp0120231/bed${i}_base_closest.bed  | sort -k1,1n  | awk '!n[$1]++' > tmp0120231/bed${i}_base_closest.tbl
+        sort -k1,1 -k2,2n  bed_base${i}_summits.bed |  closestBed -a tmp0120231/CTCF_motif.bed  -b stdin  > tmp0120231/bed_base${i}_closest.bed;
+        awk -F"\t" '{OFS="\t"; if ($6 == "+") print $4,$8-$2-8; else print $4,$2-$8+8}' tmp0120231/bed_base${i}_closest.bed  | sort -k1,1n  | awk '!n[$1]++' > tmp0120231/bed_base${i}_closest.tbl
 done
 
 if [  -d shift_table.tbl ] ; then
         /bin/cp -f shift_table.tbl shift_table_old.tbl
 fi
-echo -e "motifID\tCTCF_position\tCoh1_position\tCoh2_position" > shift_results.tbl
-paste tmp0120231/bed1_base_closest.tbl tmp0120231/bed2_base_closest.tbl tmp0120231/bed3_base_closest.tbl | awk -F"\t" '{OFS="\t"; if ($1 == $3 && $1 == $5 &&  && $2 < 200 && $4 < 200 && $6 < 200 && $2 > -200 && $4 > -200 && $6 > -200) print $1,$2,$4,$6}' >> shift_table.tbl;
-rm -rf tmp0120231
+echo -e "motifID\tCTCF_position\tCoh1_position\tCoh2_position" > shift_table.tbl;
+paste tmp0120231/bed_base1_closest.tbl tmp0120231/bed_base2_closest.tbl tmp0120231/bed_base3_closest.tbl | awk -F"\t" '{OFS="\t"; if ($1 == $3 && $1 == $5  && $2 < 200 && $4 < 200 && $6 < 200 && $2 > -200 && $4 > -200 && $6 > -200) print $1,$2,$4,$6}' >> shift_table.tbl;
+rm -rf tmp0120231 bed_base*_summits.bed
